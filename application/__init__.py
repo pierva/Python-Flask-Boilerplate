396640f29d94013260c56453efb97706bff24556
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-05-20 19:46:44 -0400

    feat: Add application error handlers

--- application/__init__.py
+++ application/__init__.py
@@ -34,3 +34,19 @@ def load_user(user_id):
         return User.query.filter(User.id == int(user_id)).first()
     except exc.SQLAlchemyError as e:
         return None
+
+
+# error handlers setup (use with abort(errCode))
+@app.errorhandler(403)
+def forbidden_page(error):
+    return render_template("errors/403.html"), 403
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("errors/404.html"), 404
+
+
+@app.errorhandler(500)
+def server_error_page(error):
+    return render_template("errors/500.html"), 500
